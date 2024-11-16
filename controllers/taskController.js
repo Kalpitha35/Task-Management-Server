f8d6c910ae01228afbf49efb0750e6b40c0f822b
Kalpitha35 <kalpithacj25@gmail.com>
2024-11-16 23:29:48 +0530

    change1

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -19,3 +19,69 @@ exports.addTaskController = async(req,res)=>{
     }
     
 }
+
+//get all task  : need authorisation
+
+exports.getTaskController = async(req,res)=>{
+    console.log("Inisde getTask");
+    const userId = req.userId
+    try{
+        const alltask = await tasks.find({userId})
+        res.status(200).json(alltask)
+    }catch(err){
+        res.status(401).json(err)
+    }
+}
+
+//delete task
+
+exports.taskDeleteController = async (req, res) => {
+    console.log("Inside task delete controller");
+    const {id} = req.params
+    console.log(`task id: ${id}`);
+    
+    try {
+        const deleteTask = await tasks.findByIdAndDelete({ _id: id })
+        res.status(200).json(deleteTask)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+//edit task
+exports.taskEditController = async (req, res) => {
+    console.log("inside taskEditController");
+    const id = req.params.id;
+    const { heading, description, startDate, endDate, status } = req.body;
+  
+    try {
+      const updatedTask = await tasks.findByIdAndUpdate(
+        { _id: id },
+        { title, description, startDate, endDate, status },
+        { new: true }
+      );
+  
+      if (!updatedTask) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+  
+      res.status(200).json(updatedTask);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  };
+  
+  //delete task
+
+exports.taskDeleteController = async (req, res) => {
+    console.log("Inside task delete controller");
+    const {id} = req.params
+    console.log(`task id: ${id}`);
+    
+    try {
+        const deleteTask = await tasks.findByIdAndDelete({ _id: id })
+        res.status(200).json(deleteTask)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
--- routes/router.js
+++ routes/router.js
@@ -13,4 +13,14 @@ router.post('/login',userController.loginController)
 //addtask
 router.post('/addtask',jwtMiddleware,taskController.addTaskController)
 
+//all-task : http://localhost:4000/all-task
+router.get('/all-task',jwtMiddleware,taskController.getTaskController)
+
+//delete-task : http://localhost:4000/delete-task
+router.delete('/tasks/:id/delete-task',jwtMiddleware,taskController.taskDeleteController)
+// update task
+router.put('/tasks/:id/update', jwtMiddleware, taskController.taskEditController)
+
+//delete-task : http://localhost:4000/delete-task
+router.delete('/tasks/:id/delete-task',jwtMiddleware,taskController.taskDeleteController)
 module.exports = router
